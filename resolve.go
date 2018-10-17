6f656e7dfd3c25adfd12964af38435df4da7e391
Ollivier Robert <roberto@keltia.net>
2018-10-17 14:39:38 +0200

    Import resolving functions from dmarc-cat.

---
+++ resolve.go
@@ -0,0 +1,26 @@
+package spf
+
+import (
+	"net"
+)
+
+// Resolver is the main interface we use
+type Resolver interface {
+	LookupAddr(addr string) ([]string, error)
+}
+
+// NullResolver is empty
+type NullResolver struct{}
+
+// LookupAddr always return a good and fixed answer
+func (NullResolver) LookupAddr(addr string) ([]string, error) {
+	return []string{addr}, nil
+}
+
+// RealResolver will call the real one
+type RealResolver struct{}
+
+// LookupAddr use the real "net" function
+func (r RealResolver) LookupAddr(addr string) ([]string, error) {
+	return net.LookupAddr(addr)
+}

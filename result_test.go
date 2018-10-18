a0bf1ec9fe704a4f9d3ccb80499fc720dc8d77e2
Ollivier Robert <roberto@keltia.net>
2018-10-18 18:21:52 +0200

    Add tests for Result.ParseTXT().

--- result_test.go
+++ result_test.go
@@ -2,8 +2,43 @@ package spf
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestResult_AppendMX(t *testing.T) {
 
+}
+
+func TestResult_ParseTXT_Empty(t *testing.T) {
+	r := Result{}
+	f, err := r.parseTXT("")
+	assert.Error(t, err)
+	assert.Nil(t, f)
+}
+
+func TestResult_ParseTXT_Good(t *testing.T) {
+	SetDebug()
+	r := Result{}
+	f, err := r.parseTXT("salesforce.com")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, f)
+	t.Logf("f=%s", f)
+	t.Logf("rec=%d", r.rec)
+	t.Logf("dns=%d", r.dns)
+	assert.Equal(t, f, r.IPs)
+	Reset()
+}
+
+func TestResult_ParseTXT_Keltia(t *testing.T) {
+	SetDebug()
+	r := Result{}
+	f, err := r.parseTXT("keltia.net")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, f)
+	t.Logf("f=%s", f)
+	t.Logf("rec=%d", r.rec)
+	t.Logf("dns=%d", r.dns)
+	assert.Equal(t, f, r.IPs)
+	Reset()
 }

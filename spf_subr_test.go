801f010b0956b023281bc52d1009fef663325cad
Ollivier Robert <roberto@keltia.net>
2018-10-18 18:21:24 +0200

    Remove TestParseTXT*, moved into result_test.go.

--- spf_subr_test.go
+++ spf_subr_test.go
@@ -62,14 +62,3 @@ func TestGetSPF_NotFirst(t *testing.T) {
 	assert.Equal(t, r2, res)
 }
 
-func TestParseTXT_Empty(t *testing.T) {
-	f, err := parseTXT("")
-	assert.Error(t, err)
-	assert.Nil(t, f)
-}
-
-func TestParseTXT_Nil(t *testing.T) {
-	f, err := parseTXT("v=spf1")
-	assert.NoError(t, err)
-	assert.Nil(t, f)
-}
